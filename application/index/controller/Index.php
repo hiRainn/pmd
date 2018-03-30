3ad844019d5288a175021bcf6048d95a7a3308f8
hiRainn <wr_sorarara@yahoo.co.jp>
2018-03-30 11:06:21 +0800

    修改数据类型为column_type

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -51,7 +51,7 @@ class Index extends Base
     {
         $table = request()->param('table');
         try{
-            $field ='COLUMN_NAME as col_name,COLUMN_DEFAULT as default_data,DATA_TYPE as data_type,IS_NULLABLE as is_null,COLUMN_COMMENT as col_comment,COLUMN_KEY as index_key';
+            $field ='COLUMN_NAME as col_name,COLUMN_DEFAULT as default_data,COLUMN_TYPE as data_type,IS_NULLABLE as is_null,COLUMN_COMMENT as col_comment,COLUMN_KEY as index_key';
             $detail = Db::query("select {$field} from INFORMATION_SCHEMA.Columns where table_name='{$table}' and table_schema= '{$this->database}'");
             
             $index = Db::query("SHOW index FROM {$table}");
